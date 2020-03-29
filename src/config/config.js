00dd3ed8bc910367ada9e613d9e5f37ba39fc8c8
Felipe Novais <novaisfelipe97@gmail.com>
2020-03-29 19:40:07 -0300

    Adicionando variaveis de ambiente

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const configs = {
-  key: "7ce76799a1c6616fa3937efb188d4be40e0dd162aaa05cc99ff0131fc543410b"
+  key: ""
 };
 
 module.exports = configs;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 mongoose.connect(
-  "mongodb://127.0.0.1:27017/acharep?gssapiServiceName=mongodb",
+  process.env.MONGO_URL,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
