f53aef5c4db1e4f25839e9c5e01e1189d499cbac
Felipe Novais <novaisfelipe97@gmail.com>
2020-04-08 21:57:06 -0300

    Alterando limite do payload

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ mongoose.connect(
 );
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(routes);
 
 app.listen(process.env.PORT || 3333);
